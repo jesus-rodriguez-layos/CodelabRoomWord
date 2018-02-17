bfcf295554635c31b9a21b2bcbdbf9cbe9aa43e7
Jesús Rodríguez <jrdev9@gmail.com>
2018-02-18 00:14:47 +0100

    Add delete and populate database when app is created

--- app/src/main/java/com/jrdev9/codelabroomword/db/WordRoomDatabase.java
+++ app/src/main/java/com/jrdev9/codelabroomword/db/WordRoomDatabase.java
@@ -1,10 +1,11 @@
 package com.jrdev9.codelabroomword.db;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.support.annotation.NonNull;
+import android.os.AsyncTask;
 
 import com.jrdev9.codelabroomword.daos.WordDao;
 import com.jrdev9.codelabroomword.models.Word;
@@ -13,6 +14,8 @@ import com.jrdev9.codelabroomword.models.Word;
 public abstract class WordRoomDatabase extends RoomDatabase {
 
     private static WordRoomDatabase INSTANCE;
+
+    public abstract WordDao wordDao();
 
     public static WordRoomDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
@@ -25,10 +28,55 @@ public abstract class WordRoomDatabase extends RoomDatabase {
         return INSTANCE;
     }
 
-    @NonNull
     private static WordRoomDatabase buildDatabase(Context context) {
-        return Room.databaseBuilder(context.getApplicationContext(), WordRoomDatabase.class, "word_database").build();
+        return Room
+                .databaseBuilder(
+                        context.getApplicationContext(),
+                        WordRoomDatabase.class,
+                        "word_database")
+                .fallbackToDestructiveMigration()
+                .addCallback(roomDatabaseCallback)
+                .build();
     }
 
-    public abstract WordDao wordDao();
+    /**
+     * Override the onOpen method to populate the database.
+     * For this sample, we clear the database every time it is created or opened.
+     */
+    private static RoomDatabase.Callback roomDatabaseCallback = new RoomDatabase.Callback() {
+
+        @Override
+        public void onOpen(SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            // If you want to keep the data through app restarts,
+            // comment out the following line.
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    /**
+     * Populate the database in the background.
+     * If you want to start with more words, just add them.
+     */
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WordDao dao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            dao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            dao.deleteAll();
+
+            Word word = new Word("Hello");
+            dao.insert(word);
+            word = new Word("World");
+            dao.insert(word);
+            return null;
+        }
+    }
 }
