8f1bd7f3f02f7fc64daf65331e43fa2608812ab1
Jesús Rodríguez <jrdev9@gmail.com>
2018-02-17 23:22:18 +0100

    Add word repository

---
+++ app/src/main/java/com/jrdev9/codelabroomword/repositories/WordRepository.java
@@ -0,0 +1,46 @@
+package com.jrdev9.codelabroomword.repositories;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.jrdev9.codelabroomword.daos.WordDao;
+import com.jrdev9.codelabroomword.db.WordRoomDatabase;
+import com.jrdev9.codelabroomword.models.Word;
+
+import java.util.List;
+
+public class WordRepository {
+
+    private final WordDao wordDao;
+    private final LiveData<List<Word>> liveDataAllWords;
+
+    private WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        wordDao = db.wordDao();
+        liveDataAllWords = wordDao.getAllWords();
+    }
+
+    private LiveData<List<Word>> getAllWords() {
+        return wordDao.getAllWords();
+    }
+
+    public void insert(Word word) {
+        new InsertAsyncTask(wordDao).execute(word);
+    }
+
+    private static class InsertAsyncTask extends AsyncTask<Word, Void, Void> {
+
+        private WordDao asyncTaskDao;
+
+        private InsertAsyncTask(WordDao dao) {
+            asyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Word... params) {
+            asyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+}
