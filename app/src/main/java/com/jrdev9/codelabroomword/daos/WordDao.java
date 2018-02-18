e0b0b721fd61d9d9cc8e3a8831ef0bbe733c9fbc
Jesús Rodríguez <jrdev9@gmail.com>
2018-02-18 12:21:33 +0100

    Fix insert word

--- app/src/main/java/com/jrdev9/codelabroomword/daos/WordDao.java
+++ app/src/main/java/com/jrdev9/codelabroomword/daos/WordDao.java
@@ -3,6 +3,7 @@ package com.jrdev9.codelabroomword.daos;
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import com.jrdev9.codelabroomword.models.Word;
@@ -12,7 +13,7 @@ import java.util.List;
 @Dao
 public interface WordDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
 
     @Query("DELETE FROM words")
